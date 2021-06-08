1794961a0793c6f4e521a0bcd4587420ad78033c
dims@calculon <dims12@mail.ru>
2021-06-08 22:07:14 +0300

    Почин

---
+++ README.md
@@ -0,0 +1,24 @@
+Перевод на русский подписей корпуса Flickr8k
+
+1) Перевод уже включён в проект
+
+2) Для самостоятельного повторения процедуры
+
+а) Скачайте корпус Flickr8k
+
+- например, командами
+
+```bash
+    wget -q https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip
+    unzip -qq Flickr8k_text.zip
+```
+
+б) Создайте себе доступ к Cloud Translate API, в частности
+
+- это должно привести к скачиванию JSON файла куда-то на локальный компьютер
+
+- укажите путь к этому файлу в переменной среды `GOOGLE_APPLICATION_CREDENTIALS`
+
+- подробности должны быть на [страницах документации Гугл](https://cloud.google.com/translate/docs/setup#creating_service_accounts_and_keys).
+
+У меня процедура перевода заняла около 1 часа.
---
+++ translate3.py
@@ -0,0 +1,33 @@
+from google.cloud import translate_v2 as translate
+
+from tqdm import tqdm
+
+import os
+
+FLICKR_PATH = '/NAS/data/SpeechCorpora/Flickr8k'
+
+INPUT_FILENAME = 'Flickr8k.token.txt'
+# INPUT_FILENAME = 'Flickr8k.lemma.token.txt'
+OUTPUT_FILENAME = 'Flickr8k.token.ru.txt'
+
+
+def main():
+    input_filepath = os.path.join(FLICKR_PATH, INPUT_FILENAME)
+    output_filepath = os.path.join(OUTPUT_FILENAME)
+
+    translator = translate.Client()
+
+    with open(input_filepath) as tokenfile:
+        with open(output_filepath, "w") as translated_file:
+            tokenlines = tokenfile.readlines()
+            for line in tqdm(tokenlines):
+                img_name, caption = line.split("\t")
+                caption = caption.strip()
+
+                result = translator.translate(caption, target_language="ru", source_language="en")
+                caption_ru = result['translatedText']
+                translated_file.write("%s\t%s\n" % (img_name, caption_ru))
+
+
+if __name__ == '__main__':
+    main()
